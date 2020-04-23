b193dae8da9dec4d99ec151a25366fb166428b5d
Ross Hazelett <rhhazelett@gmail.com>
2020-04-23 17:13:31 -0400

    added getShouts function, deployed to firebase, working on Postman

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+exports.getShouts = functions.https.onRequest((req, res) => {
+  admin.firestore().collection('shouts').get()
+    .then((data) => {
+      let shouts = [];
+      data.forEach((doc) => {
+        shouts.push(doc.data());
+      });
+      return res.json(shouts);
+    })
+    .catch((err) => console.error(err));
+})
