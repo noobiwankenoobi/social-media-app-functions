76b7a0a04bd0c8bfec10f2dd6c09e89cf2df541c
Ross Hazelett <rhhazelett@gmail.com>
2020-04-25 12:51:22 -0400

    installed firebase client library to functions directory, created a POST to signup route, verified it works in Postman

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  shouts: [
+    {
+      userHandle: 'user',
+      body: 'this is the shout body',
+      createdAt: '2020-04-25T15:57:20.980Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
--- functions/index.js
+++ functions/index.js
@@ -6,38 +6,85 @@ admin.initializeApp();
 const express = require('express');
 const app = express();
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDC_esLx9natikWK59l6BRHy7sZrrhw1L8",
+  authDomain: "social-media-app-v01.firebaseapp.com",
+  databaseURL: "https://social-media-app-v01.firebaseio.com",
+  projectId: "social-media-app-v01",
+  storageBucket: "social-media-app-v01.appspot.com",
+  messagingSenderId: "1063384140539",
+  appId: "1:1063384140539:web:1382d9ca6abd37de5efe5e",
+  measurementId: "G-ZRD24CNTWZ"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+// GET Shouts Route
 // first parameter is the route, second is the handler
 app.get('/shouts', (req, res) => {
-  admin.firestore().collection('shouts').get()
+  admin
+    .firestore()
+    .collection('shouts')
+    .get()
     .then((data) => {
       let shouts = [];
       data.forEach((doc) => {
-        shouts.push(doc.data());
-      });
+        shouts.push({
+          shoutId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+          commentCount: doc.data().commentCount,
+          likeCount: doc.data().likeCount
+        });
+      })
       return res.json(shouts);
     })
     .catch((err) => console.error(err));
 })
 
+// POST Shout Route
 app.post('/shout', (req, res) => {
   const newShout = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
 
   admin
     .firestore()
     .collection('shouts')
+    .orderBy('createdAt', 'desc')
     .add(newShout)
-    .then(doc => {
+    .then((doc) => {
       res.json({ message: `document ${doc.id} created successfully` });
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json({ error: 'something went wrong' });
       console.error(err);
     })
 });
 
+// Signup Route (POST)
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then((data) => {
+      return res.status(200).json({ message: `user ${data.user.uid} signed up successfully` });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    })
+});
 
 exports.api = functions.https.onRequest(app);
