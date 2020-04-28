cb34b2e6a6ff8d9baf3098f16def6e9a71b2fbab
Ross Hazelett <rhhazelett@gmail.com>
2020-04-28 16:42:58 -0400

    finished restructuring code, much cleaner, working in postman

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyDC_esLx9natikWK59l6BRHy7sZrrhw1L8",
+  authDomain: "social-media-app-v01.firebaseapp.com",
+  databaseURL: "https://social-media-app-v01.firebaseio.com",
+  projectId: "social-media-app-v01",
+  storageBucket: "social-media-app-v01.appspot.com",
+  messagingSenderId: "1063384140539",
+  appId: "1:1063384140539:web:1382d9ca6abd37de5efe5e",
+  measurementId: "G-ZRD24CNTWZ"
+};
